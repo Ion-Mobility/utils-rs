787197957207f9d32ffa1b74fdc61806ea7e053d
Saydi <truong@ionmobility.com>
2024-08-08 09:51:42 +0700

    update utils-rs for recovering can socket

--- cantool/src/can_tool.rs
+++ cantool/src/can_tool.rs
@@ -13,16 +13,26 @@ use std::path::Path;
 #[derive(Debug)]
 pub struct CanUtils {
     hash_msg: HashMap<String, u32>,
+    canport: String,
     socket_can: CanSocket,
     can_info: PgnLibrary,
     id_and_signal: HashMap<u32, Vec<String>>,
+    can_filter_names: Vec<CanFilter>,
 }
 
 impl CanUtils {
-    pub fn new<P: AsRef<Path>>(dbcpath: P, canport: &str) -> Result<Self, Box<dyn Error>> {
-        let dbcpath_str = dbcpath.as_ref().to_str().ok_or("Invalid DBC path")?.to_string();
+    pub fn new<P: AsRef<Path>>(dbcpath: P, canport: &str, canfilter_names: &[&str]) -> Result<Self, Box<dyn Error>> {
+        let dbcpath_str = match dbcpath.as_ref().to_str() {
+            Some(s) => s.to_string(),
+            None => return Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid DBC path"))),
+        };
+
         let mut hash_msg = HashMap::new();
-        let can_info = PgnLibrary::from_dbc_file(&dbcpath_str)?;
+        let can_info = match PgnLibrary::from_dbc_file(&dbcpath_str) {
+            Ok(info) => info,
+            Err(e) => return Err(Box::new(e)),
+        };
+
         let id_and_signal = can_info
             .hash_of_canid_signals()
             .into_iter()
@@ -30,31 +40,64 @@ impl CanUtils {
             .collect::<HashMap<u32, Vec<String>>>();
 
         // Read the DBC file and populate hash_msg
-        let file = File::open(&dbcpath)?;
+        let file = match File::open(&dbcpath) {
+            Ok(f) => f,
+            Err(e) => return Err(Box::new(e)),
+        };
         let reader = BufReader::new(file);
-
         for line in reader.lines() {
-            let line = line?;
-            if line.starts_with("BO_ ") {
-                let parts: Vec<&str> = line.split_whitespace().collect();
-                if parts.len() > 2 {
-                    if let Ok(id) = parts[1].parse::<u32>() {
-                        let name = parts[2].trim_end_matches(':').to_string();
-                        hash_msg.insert(name, id);
+            match line {
+                Ok(l) => {
+                    if l.starts_with("BO_ ") {
+                        let parts: Vec<&str> = l.split_whitespace().collect();
+                        if parts.len() > 2 {
+                            if let Ok(id) = parts[1].parse::<u32>() {
+                                let name = parts[2].trim_end_matches(':').to_string();
+                                hash_msg.insert(name, id);
+                            }
+                        }
                     }
-                }
+                },
+                Err(e) => return Err(Box::new(e)),
+            }
+        }
+
+        let socket_can = match CanSocket::open(canport) {
+            Ok(s) => s,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        let mut can_filters = Vec::new();
+        for &name in canfilter_names {
+            if let Some(&id) = hash_msg.get(name) {
+                let extended_id = id & 0x1FFFFFFF; // Apply the extended ID bit
+                let filter = CanFilter::new(extended_id, 0x1FFFFFFF);
+                info!(
+                    "Mapping: {} -----> To Can ID: {} (Decimal) ----> {:#X} (Hex) ----> Extended ID: {:#X}",
+                    name, id, id, extended_id
+                );
+                can_filters.push(filter);
+            } else {
+                error!("Error: CAN name '{}' not found in the database", name);
+            }
+        }
+
+        if !can_filters.is_empty() {
+            if let Err(e) = socket_can.set_filters(can_filters.as_slice()) {
+                return Err(Box::new(e));
             }
         }
 
         Ok(CanUtils {
             hash_msg,
-            socket_can: CanSocket::open(canport)?,
+            canport: canport.to_string(),
+            socket_can,
             can_info,
             id_and_signal,
+            can_filter_names: can_filters
         })
     }
 
-    // Function to get CAN IDs from a list of CAN names.
     pub fn get_can_ids_from_can_names(&self, can_names: &[&str]) -> Vec<u32> {
         let mut can_ids = Vec::new();
         for &name in can_names {
@@ -73,7 +116,6 @@ impl CanUtils {
         can_ids
     }
 
-    // Function to get a CAN ID from a CAN name.
     pub fn get_can_id_from_can_name(&self, can_name: String) -> u32 {
         let can_name_str = can_name.as_str();
         if let Some(&id) = self.hash_msg.get(can_name_str) {
@@ -106,11 +148,13 @@ impl CanUtils {
         }
 
         if !can_filters.is_empty() {
-            self.socket_can.set_filters(can_filters.as_slice()).unwrap();
+            if let Err(e) = self.socket_can.set_filters(can_filters.as_slice()) {
+                error!("Failed to set CAN filters: {:?}", e);
+            }
         }
     }
 
-    pub fn get_signals(&self) -> Result<HashMap<String, f32>, Box<dyn Error>> {
+    pub fn get_signals(&mut self) -> Result<HashMap<String, f32>, Box<dyn Error>> {
         let mut result = HashMap::new();
 
         match self.socket_can.read_frame() {
@@ -140,7 +184,24 @@ impl CanUtils {
             }
             Err(e) => {
                 log::error!("Failed to read CAN frame: {:?}", e);
-                return Err(Box::new(e));
+                log::info!("Attempting to reopen CanSocket...");
+
+                // Attempt to reopen the CanSocket
+                match CanSocket::open(&self.canport) {
+                    Ok(new_socket) => {
+                        self.socket_can = new_socket;
+                        log::info!("Successfully reopened CanSocket.");
+                        if !self.can_filter_names.is_empty() {
+                            if let Err(e) = self.socket_can.set_filters(self.can_filter_names.as_slice()) {
+                                return Err(Box::new(e));
+                            }
+                        }
+                    }
+                    Err(reopen_error) => {
+                        log::error!("Failed to reopen CanSocket: {:?}", reopen_error);
+                        return Err(Box::new(reopen_error));
+                    }
+                }
             }
         }
 
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,14 @@ fn main() {
     console_log.init_logger();
     
     // Initialize CAN communication
-    let can_conn = CanUtils::new("/usr/share/can-dbcs/consolidated.dbc".to_string(), "vcan0");
-    
     let can_filters: Vec<&str> = vec!["vcu_ble_pkt_1"];
-    can_conn.as_ref().expect("Failed to initialize CAN connection").set_can_filters_from_can_names(&can_filters);
+    let mut can_conn = match CanUtils::new("/usr/share/can-dbcs/consolidated.dbc", "can0", &can_filters) {
+        Ok(conn) => conn,
+        Err(e) => {
+            info!("Failed to initialize CAN connection: {}", e);
+            return;
+        }
+    };
 
     // Initialize modem client
     let mut modem_cli = IonModemCli::default();
@@ -27,10 +31,10 @@ fn main() {
     // Main loop
     loop {
         // Read CAN signals
-        match can_conn.as_ref().expect("Failed to read CAN signals").get_signals() {
-            Ok(_signal) => {
-                trace!("CAN Signal: {:?}", _signal);
-                for (signal, value) in _signal {
+        match can_conn.get_signals() {
+            Ok(signals) => {
+                info!("CAN Signal: {:?}", signals);
+                for (signal, value) in signals {
                     match signal.as_str() {
                         "ble_cellular" => {
                             vehicle_cell_enable = value != 0.0;
@@ -50,56 +54,56 @@ fn main() {
         }
 
         // Check if modem is ready
-        if modem_cli.waiting_for_ready() {
-            info!("Location Enabled: {}, Modem Enabled: {}, Signal Strength: {}", 
-                modem_cli.is_location_enabled(), modem_cli.is_modem_enabled(), modem_cli.get_signal_strength());
+        // if modem_cli.waiting_for_ready() {
+        //     info!("Location Enabled: {}, Modem Enabled: {}, Signal Strength: {}", 
+        //         modem_cli.is_location_enabled(), modem_cli.is_modem_enabled(), modem_cli.get_signal_strength());
 
-            // Enable modem if not already enabled
-            if !modem_cli.is_modem_enabled() {
-                match modem_cli.setup_modem_enable(true) {
-                    Ok(_) => {
-                        trace!("Modem enabled successfully");
-                    }
-                    Err(e) => {
-                        info!("Failed to enable modem: {:?}", e);
-                    }
-                }
-            }
+        //     // Enable modem if not already enabled
+        //     if !modem_cli.is_modem_enabled() {
+        //         match modem_cli.setup_modem_enable(true) {
+        //             Ok(_) => {
+        //                 trace!("Modem enabled successfully");
+        //             }
+        //             Err(e) => {
+        //                 info!("Failed to enable modem: {:?}", e);
+        //             }
+        //         }
+        //     }
 
-            // Handle GPS based on user settings
-            if vehicle_gps_enable {
-                trace!("Enabling GPS based on user setting");
-                if !modem_cli.is_location_enabled() {
-                    match modem_cli.setup_location(0x07, true) {
-                        Ok(_) => {
-                            trace!("Location enabled successfully");
-                        }
-                        Err(e) => {
-                            info!("Failed to enable location: {:?}", e);
-                        }
-                    }
-                }
-            } else {
-                if modem_cli.is_location_enabled() {
-                    match modem_cli.setup_location(0x03, true) {
-                        Ok(_) => {
-                            trace!("Location disabled successfully");
-                        }
-                        Err(e) => {
-                            info!("Failed to disable location: {:?}", e);
-                        }
-                    }
-                }
-            }
+        //     // Handle GPS based on user settings
+        //     if vehicle_gps_enable {
+        //         trace!("Enabling GPS based on user setting");
+        //         if !modem_cli.is_location_enabled() {
+        //             match modem_cli.setup_location(0x07, true) {
+        //                 Ok(_) => {
+        //                     trace!("Location enabled successfully");
+        //                 }
+        //                 Err(e) => {
+        //                     info!("Failed to enable location: {:?}", e);
+        //                 }
+        //             }
+        //         }
+        //     } else {
+        //         if modem_cli.is_location_enabled() {
+        //             match modem_cli.setup_location(0x03, true) {
+        //                 Ok(_) => {
+        //                     trace!("Location disabled successfully");
+        //                 }
+        //                 Err(e) => {
+        //                     info!("Failed to disable location: {:?}", e);
+        //                 }
+        //             }
+        //         }
+        //     }
 
-            // Handle cellular data based on user settings (currently traced only)
-            if vehicle_cell_enable {
-                trace!("Enabling Data LTE based on user setting");
-            }
+        //     // Handle cellular data based on user settings (currently traced only)
+        //     if vehicle_cell_enable {
+        //         trace!("Enabling Data LTE based on user setting");
+        //     }
 
-        } else {
-            info!("Modem is not ready");
-        }
+        // } else {
+        //     info!("Modem is not ready");
+        // }
 
         // Sleep for a short duration before looping again
         thread::sleep(Duration::from_millis(100));
