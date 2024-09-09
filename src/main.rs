ad8ad7754cfd0d357eb9933169f5334db4568fd9
Saydi <truong@ionmobility.com>
2024-09-09 13:41:24 +0700

    update to rename icomconn to icommsg

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
-use std::thread;
-use std::time::Duration;
 use log::{trace, info};
 use mmcli::mmcli::IonModemCli; // Assuming IonModemCli is imported from mmcli crate
 use cantool::can_tool::CanUtils; // Assuming CanUtils is imported from cantool crate
 use logging::logging::MyLogging; // Assuming MyLogging is imported from logging crate
 use spiconn::spi_conn::*;
 use tokio::io;
-use wifitools::{scan_wifi, get_stored_wifi};
+use tokio::time::{sleep, Duration};
+use wifitools::{scan_wifi, get_stored_wifi, get_wificmd_pack, send_wificmd_pack};
 use icomconn::icom_conn::IONICOMPacketType;
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    // Initialize IonSpiConn asynchronously
-    if let  Ok(mut spi_conn) = IonSpiConn::new_async("/dev/spidev1.0", 29).await {
-        loop {
-            // Example data to send over SPI
-            let tx_data = [0xAA; 128];
-            let txbuf = IONICOMPacketType::new_from(tx_data.to_vec());
-            // Perform the SPI transfer and handle the result
-            match spi_conn.xfer(&txbuf.to_byte_array()).await {
-                Ok(rx_data) => {
-                    // println!("Received data: {:?}", rx_data);
-                    match IONICOMPacketType::from_byte_array(rx_data) {
-                        Ok(_icom_rx) => {
-                            println!("{:?}", _icom_rx);
-                        }
-                        Err(e) => {
-                            eprintln!("error: e");
-                        }
-                    }
-                }
-                Err(e) => {
-                    eprintln!("SPI transfer failed: {:?}", e);
-                }
-            }
+    loop {
+        if let Ok(_wifi_cmd) = get_wificmd_pack().await {
+            println!("Found wifi command package: {:?}", _wifi_cmd);
+            let _ = send_wificmd_pack(_wifi_cmd).await;
         }
+        sleep(Duration::from_millis(100)).await;
     }
-    Ok(())
 }
