167848aebde67b790d692be37506fb31ad651027
Saydi <truong@ionmobility.com>
2024-09-19 15:23:06 +0700

    add more 4 api for ota send/recv

--- gwclient/example/gwclient-test/src/main.rs
+++ gwclient/example/gwclient-test/src/main.rs
@@ -1,31 +1,68 @@
-use gwclient::{get_ota_message};
+use gwclient::{
+    get_ota_pub_message,
+    get_ota_sub_message,
+    send_ota_pub_message,
+    send_ota_sub_message
+};
 use tokio::time::{sleep, Duration};
 
-// async fn stub_send_ota_message() {
-//     println!("Send Thread");
-//     loop {
-//         match send_ota_message("Stub something to send".as_bytes().to_vec()).await {
-//             Ok(_result) => {
-//                 println!("Success");
-//             }
-//             Err(e) => {
-//                 eprintln!("Can't send ota package {}", e);
-//             }
-//         }
-//         sleep(Duration::from_millis(1000)).await;
-//     }
-// }
-
-async fn stub_recv_ota_message() {
-    println!("Recv Thread");
+async fn stub_send_ota_sub_message() {
+    println!("Send Thread");
     loop {
-        let _result = get_ota_message().await;
-        if _result.len() > 0 {
-            println!("Recv success {:?}", String::from_utf8(_result));
-        } else {
-            eprintln!("Can't send ota package");
+        match send_ota_sub_message("Stub something to send on MQTT Sub Channel".as_bytes().to_vec()).await {
+            Ok(_result) => {
+                println!("Send Success");
+            }
+            Err(e) => {
+                eprintln!("Can't send ota package {}", e);
+            }
         }
         sleep(Duration::from_millis(1000)).await;
+    }
+}
+
+async fn stub_recv_ota_sub_message() {
+    println!("Recv Thread");
+    loop {
+        match get_ota_sub_message().await {
+            Ok(_result) => {
+                println!("Recv success {:?}", String::from_utf8(_result));
+            }
+            _ => {
+
+            }
+        }
+        sleep(Duration::from_millis(500)).await;
+    }
+}
+
+async fn stub_send_ota_pub_message() {
+    println!("Send Thread");
+    loop {
+        match send_ota_sub_message("Stub something to send on MQTT Pub Channel".as_bytes().to_vec()).await {
+            Ok(_result) => {
+                println!("Send Success");
+            }
+            Err(e) => {
+                eprintln!("Can't send ota package {}", e);
+            }
+        }
+        sleep(Duration::from_millis(1000)).await;
+    }
+}
+
+async fn stub_recv_ota_pub_message() {
+    println!("Recv Thread");
+    loop {
+        match get_ota_sub_message().await {
+            Ok(_result) => {
+                println!("Recv success {:?}", String::from_utf8(_result));
+            }
+            _ => {
+
+            }
+        }
+        sleep(Duration::from_millis(500)).await;
     }
 }
 
@@ -33,8 +70,10 @@ async fn stub_recv_ota_message() {
 async fn main() {
     tokio::spawn(async move {
         tokio::join!(
-            // stub_send_ota_message(),
-            stub_recv_ota_message(),
+            stub_send_ota_sub_message(),
+            stub_recv_ota_sub_message(),
+            stub_send_ota_pub_message(),
+            stub_recv_ota_pub_message(),
         );
     });
     loop {
--- gwclient/src/lib.rs
+++ gwclient/src/lib.rs
@@ -1,14 +1,7 @@
-use rusty_network_manager::{
-    AccessPointProxy, NetworkManagerProxy, SettingsConnectionProxy, SettingsProxy, WirelessProxy, DeviceProxy
-};
-// use zbus::zvariant::{OwnedValue, Value as ZValue};
-use std::collections::HashMap;
-use tokio::time::{sleep, Duration, Instant};
-use zbus::zvariant::Value;
 use zbus::{Connection, Proxy};
-use zvariant::{ObjectPath, OwnedValue, Str};
+use zbus::fdo::Result;
 
-pub async fn get_ota_message() -> Vec<u8> {
+pub async fn get_ota_pub_message() -> Result<Vec<u8>> {
     // Create a connection to the system bus
     let mut _result: Vec<u8> = Vec::new();
     if let Ok(connection) = Connection::system().await {
@@ -19,7 +12,7 @@ pub async fn get_ota_message() -> Vec<u8> {
             "org.ion.IComGateway",  // Introspection interface
         )
         .await {
-            if let Ok(received_pack) = proxy.call("RecvOtaMessage", &()).await {
+            if let Ok(received_pack) = proxy.call("RecvOtaPubMessage", &()).await {
                 _result = received_pack;
             }
 
@@ -27,24 +20,65 @@ pub async fn get_ota_message() -> Vec<u8> {
     }
 
     // Call the `Introspect` method to retrieve introspection XML
-    _result
+    Ok(_result)
 }
 
-// pub async fn send_ota_message(send_pack: Vec<u8>) -> bool {
-//     let mut result = false;
-//     // Create a connection to the system bus
-//     if let Ok(connection) = Connection::system().await {
-//         // Create a proxy for interacting with the D-Bus service
-//         if let Ok(proxy) = Proxy::new(
-//             &connection,
-//             "org.ion.IComGateway",  // D-Bus destination (service name)
-//             "/org/ion/IComGateway", // Object path
-//             "org.ion.IComGateway",  // Introspection interface
-//         )
-//         .await {
-//             // Call the D-Bus method and check for a successful response
-//             proxy.call("SendOtaMessage", &(send_pack)).await;
-//         }
-//     }
-//     result
-// }
+pub async fn get_ota_sub_message() -> Result<Vec<u8>> {
+    // Create a connection to the system bus
+    let mut _result: Vec<u8> = Vec::new();
+    if let Ok(connection) = Connection::system().await {
+        if let Ok(proxy) = Proxy::new(
+            &connection,
+            "org.ion.IComGateway",  // D-Bus destination (service name)
+            "/org/ion/IComGateway", // Object path
+            "org.ion.IComGateway",  // Introspection interface
+        )
+        .await {
+            if let Ok(received_pack) = proxy.call("RecvOtaSubMessage", &()).await {
+                _result = received_pack;
+            }
+
+        }
+    }
+
+    // Call the `Introspect` method to retrieve introspection XML
+    Ok(_result)
+}
+
+pub async fn send_ota_pub_message(data: Vec<u8>) -> Result<bool> {
+    // Create a connection to the system bus
+    let mut _result = false;
+    if let Ok(connection) = Connection::system().await {
+        if let Ok(proxy) = Proxy::new(
+            &connection,
+            "org.ion.IComGateway",  // D-Bus destination (service name)
+            "/org/ion/IComGateway", // Object path
+            "org.ion.IComGateway",  // Introspection interface
+        )
+        .await {
+            proxy.call("SendOtaPubMessage", &(data)).await?;
+        }
+    }
+
+    // Call the `Introspect` method to retrieve introspection XML
+    Ok(_result)
+}
+
+pub async fn send_ota_sub_message(data: Vec<u8>) -> Result<bool> {
+    // Create a connection to the system bus
+    let mut _result = false;
+    if let Ok(connection) = Connection::system().await {
+        if let Ok(proxy) = Proxy::new(
+            &connection,
+            "org.ion.IComGateway",  // D-Bus destination (service name)
+            "/org/ion/IComGateway", // Object path
+            "org.ion.IComGateway",  // Introspection interface
+        )
+        .await {
+            proxy.call("SendOtaSubMessage", &(data)).await?;
+        }
+    }
+
+    // Call the `Introspect` method to retrieve introspection XML
+    Ok(_result)
+}
