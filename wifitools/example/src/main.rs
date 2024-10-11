48a8663c0b54b2cf80bd97d68bc14a929c21fa49
Saydi <truong@ionmobility.com>
2024-10-11 12:33:58 +0700

    update to add example to use get ap info

--- wifitools/example/src/main.rs
+++ wifitools/example/src/main.rs
@@ -1,4 +1,4 @@
-use wifitools::{connect_wifi,remove_stored_wifi, get_stored_wifi};
+use wifitools::{connect_wifi,remove_stored_wifi, get_stored_wifi, get_ap_info};
 use tokio::time::Duration;
 
 #[tokio::main]
@@ -6,11 +6,9 @@ async fn main() {
     // connect_wifi("wlan0", "VIETTEL_AP_8CE000", Some("1234567890a"), Duration::from_secs(10)).await.unwrap();
     // connect_wifi("wlan0", "Ion Mobility VN", Some("Imv@104A"), Duration::from_secs(10)).await.unwrap();
     // remove_stored_wifi("VIETTEL_AP_8CE000".to_string()).await.unwrap();
-    match get_stored_wifi().await {
-        Ok(ap_list) => {
-            // Make V2X Message           
-            let wifi_info = ap_list.lock().await;
-            println!("Infor: {:?}", wifi_info);
+    match get_ap_info("wlp0s20f3").await {
+        Ok((_ssid, _info)) => {
+            println!("{} {:?}", _ssid, _info);
         }
         Err(e) => {
             eprintln!("Error {}", e);
