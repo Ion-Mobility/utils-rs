820804447aaedfa9af93bcb8085288d598218a5a
Saydi <truong@ionmobility.com>
2025-08-04 16:13:24 +0700

    change version to fixed array 8bytes

--- gwclient/examples/get-isysinfo.rs
+++ gwclient/examples/get-isysinfo.rs
@@ -1,9 +1,8 @@
 use gwclient::{get_isys_info};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    loop {
-        let isysinfo = get_isys_info().await;
+    if let Ok(isysinfo) = get_isys_info().await {
         println!("Result: {:?}", isysinfo);
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     }
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 }
